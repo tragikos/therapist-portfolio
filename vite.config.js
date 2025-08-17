d92995e5f7f4e493e1f1369fc67667e695a1066a
tragikos <thodoris.k@gmail.com>
2025-08-17 13:46:11 +0300

    echoing build assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Use relative paths for all built asset URLs to work on GitHub Pages subpaths
+  // Force relative asset URLs so GitHub Pages subpaths work
   base: './',
 })
